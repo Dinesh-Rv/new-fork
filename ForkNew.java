2c9f78c099ad0b910eb81218e0c538eab605eed3
Dinesh-Rv <dineshkumar.ravikumar@ideas2it.com>
2022-12-12 15:57:59 +0530

    the main

--- ForkNew.java
+++ ForkNew.java
@@ -1,1 +1,8 @@
-hello guys 
+public class SumOfNumbers1  
+{  
+public static void main(String args[])   
+{  
+int n1 = 25, n2 = 115, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
